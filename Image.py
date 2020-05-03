f14b952ccee027244967952a4b15bd0327ef1b8e
Takis <takis.panagiot1@gmail.com>
2020-05-03 18:59:50 +0300

    no opencv,new clipboard mechanism

--- Image.py
+++ Image.py
@@ -28,43 +28,41 @@ def main():
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread("images/chaosOrig.png", 0)
 
-    w, h = template.shape[::-1]
-    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2) #draw boxes around found occurrences
-        center_x = pt[0] + w/2
-        center_y = pt[1] + h/2
-        count += 1
-        pyautogui.moveTo(center_x+window[1], center_y+window[2])
-        #ctrl c
-        pyautogui.hotkey('ctrl', 'c')
-        # pyautogui.keyDown('ctrl')
-        # pyautogui.keyDown('c')
-        # pyautogui.keyUp('c')
-        # pyautogui.keyUp('ctrl')
-        if clip_change():
-        #get clipboard content
-            clipb = Tk()
-            result = clipb.selection_get(selection="CLIPBOARD")
-            #get total currency
-            currency["chaos"] += get_currency_stack(result)
-        #sleep(0.5)
+    # center_x = pt[0] + w/2
+    # center_y = pt[1] + h/2
+    count = 0
+    cell_distance=30
+    for i in range(12):
+        for j in range(5):
+            position=[window[1] + (i*cell_distance), window[2] + (j*cell_distance)]
+            next_position=[window[1] + (i+1*cell_distance), window[2] + (j+1*cell_distance)]
+
+            result=pyperclip.copy('') # empty the string
+
+            pyautogui.moveTo(position[0], position[1])
+            #ctrl c
+            pyautogui.hotkey('ctrl', 'c')
+            # pyautogui.keyDown('ctrl')
+            # pyautogui.keyDown('c')
+            # pyautogui.keyUp('c')
+            # pyautogui.keyUp('ctrl')
+            #get clipboard content
+            try:
+                result = Tk().clipboard_get()
+            except:
+                selection = None
+            if result: #string not empty
+                #get total currency
+                last_currency= get_info(result)
+                currency["chaos"] += last_currency
+                #sleep(0.5)
     cv2.imwrite('res.png', img_rgb)
     print("Found", count, "Total currency: ", currency["chaos"])
 
-def get_currency_stack(string):
+def get_info(string):
     stack_text = re.findall("\d+\/\d+", string)[0].split("/")[0]
-    print(int(stack_text))
+    print("Found :",int(stack_text))
     return (int(stack_text))
-
-def clip_change():
-    recent_value = ""
-    #pyautogui.hotkey('ctrl','c')
-    while True:
-        tmp_value = pyperclip.paste()
-        if tmp_value != recent_value:
-            recent_value = tmp_value
-            return True
 
 def get_trade_window():
     topLeft = pyautogui.locateOnScreen('images/topLeft.png')
@@ -73,7 +71,7 @@ def get_trade_window():
     bottomRightP = pyautogui.center(bottomRight)
     img = pyautogui.screenshot(region=(topLeftP[0], topLeftP[1]+20, (bottomRightP[0]-topLeftP[0])+10, (bottomRightP[1]-topLeftP[1])-20))
     img.save("trade_window.png")
-    return [img, topLeftP[0], topLeftP[1]+20]
+    return [img, topLeftP[0]+20, topLeftP[1]+35] #image, first cell (x,y)
 
 # def currency_update(currency):
 
